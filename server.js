fdfdbf43e7505cf6ce4866c40bbbeb2fd9e3082f
LucasSilva1207 <lucas.luc1207@gmail.com>
2023-12-06 20:14:38 -0300

    Prova de BackEnd segunda avaliação

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import app from './src/app.js' 
-const port = process.env.PORT || 3000; 
+import app from './src/app.js'
+const port = process.env.PORT || 3000;
 
-
-app.listen(port, () => {
-    console.log(`Servidor escutando em http://localhost:${port}`)
-  })
+app.listen(port, () =>{
+    console.log( `servidor escutando em http://localhost:${port}`)
+});
